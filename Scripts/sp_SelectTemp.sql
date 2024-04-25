7613a8118a3adfb36e4dade01df79ea94b602cf0
IndigoJ <ko.telizhenko@gmail.com>
2024-04-25 14:20:05 +0200

    Update sp_SelectTemp.sql
    HHHHH

--- Scripts/sp_SelectTemp.sql
+++ Scripts/sp_SelectTemp.sql
@@ -2,6 +2,6 @@ DROP PROCEDURE IF EXISTS sp_SelectTemp;
 DELIMITER &&  
 CREATE PROCEDURE sp_SelectTemp ()  
 BEGIN  
-    SELECT * from testTable limit 7;   
+    SELECT * from testTable limit 3;   
 END &&  
 DELIMITER ;
